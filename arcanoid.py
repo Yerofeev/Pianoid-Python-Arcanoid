6fe2324685139c0b64f2a9c83d2b12649cd21344
Erofeev Egor <illuziya@mail.ru>
2018-02-11 13:26:23 +0300

    tricky dilemma

--- arcanoid.py
+++ arcanoid.py
@@ -36,22 +36,49 @@ def getChar():
             if answer == 'q':
                 print('Quit')
                 sleep(0.2)
-                sys.exit()                       
+                sys.exit()                 
             else:  break
         finally:
             termios.tcflush(sys.stdin, termios.TCIOFLUSH)
             termios.tcsetattr(fd, termios.TCSADRAIN, oldSettings)
     return answer	
 
-def bricks(level_file):
-    for j in range(4):    
-        for i in range(11):
-                print_there (  2+i*6, 1+j*2,  '\033[96m' + chr(9604)*5 + ' ')
-                print_there (  2+i*6, 2+j*2,  '\033[96m' + chr(9608)*5 + ' ')        #chr(9608)'█'
-    objects_x = [x for x in range(70) if (x-1)%6 != 0]                                  #Why here?????????????????
-    objects_y = [y+1 for y in range(8)]
-    return objects_x, objects_y            
+class Bricks():
+    
+    def __init__(self):
+        Bricks.obj_x = [x for x in range(2,62) if x not in range(7,62,6)]
+        Bricks.obj_y = [y for y in range(1,9)]  
+        self.status = 'ON' 
+    
+    def paint_bricks(self, i, mode=0):
+        if mode != 0:
+            if i == 39:
+                print('39 now',end='')
+            self.status = 'OFF'
+            j = i // 10
+            i = i %  10
+            print_there (2+i*6, 1+j*2,  '\033[96m' + ' '*6)
+            print_there (2+i*6, 2+j*2,  '\033[96m' + ' '*6)        #chr(9608)'█'    
 
+            #for k in range (2+i%10*6,i%10*6+6):
+            #   Bricks.obj_x.remove(k)
+            #Bricks.obj_y.remove((i//10+1)*2); Bricks.obj_y.remove((i//10+1)*2-1)            
+            return
+        j = i // 10
+        i = i %  10
+        print_there (2+i*6, 1+j*2,  '\033[96m' + chr(9604)*5 + ' ')
+        print_there (2+i*6, 2+j*2,  '\033[96m' + chr(9608)*5 + ' ')        #chr(9608)'█'        
+        return                                      #is it possible to ?
+    
+    def delete_brick(self,x,y,i):
+
+        print_there (-2+i%10*7, (i//10+1)*2-1,  ' '*6)
+        print_there (-2+i%10*7, (i//10+1)*2,  ' '*6)        
+        #print((i//10+1)*2,(i//10+1)*2-1)
+        #sleep(30)
+        return
+        
+        
 class Paddle:
 
     def __init__(self, length, lives, position):
@@ -70,57 +97,60 @@ class Paddle:
         for i in range(self.length): 
             print_there(self.position+i, 22, '\033[32m' + chr(10735) )                 #chr(8718) ∎  #chr(10735)⧯       chr(9209) ' ⏹'
         print_there(  70, 22, '\033[91m' + '❤ '*self.lives) #FINISH LATER!!! 
-
+        
 
 class Ball:
 
-    def __init__(self,x,y,speed):
+    def __init__(self, x, y, speed):
         self.x = x
         self.y = y
         self.speed = speed
         
-    def ball_deamon(self,objects_x, objects_y):
-        self.threading = threading.Thread(target=self.ball_display, args=(self.x,self.y,self.speed,objects_x, objects_y))
+    def ball_deamon(self, bricks):
+        self.threading = threading.Thread(target=self.ball_display, args=(self.x, self.y, self.speed, bricks))
         self.threading.daemon = True
         self.threading.start()       
         
-    def ball_display(self,x,y,speed,objects_x, objects_y):  
+    def ball_display(self,x,y,speed, bricks):  
         i, j = 1, -1
-        while True:
-            while x != 0 and y != 0 and x != 70 and y != 22:
-                print_there(x, y, '\033[93m' + '⏺')
-                sleep(0.1)
-                print_there( x, y, '\033[93m' + ' ')        
-                x += i
-                y += j   
-                #print(objects_y,y-1)
-                if y-1 in objects_y:
-                    num_obj = x//6
-                    print_there (2+num_obj*6, y-1,  ' '*6)        
-                    print_there (2+num_obj*6, y-2,  ' '*6)        #chr(9608)'█'   
-                    j=1
-                    x+=1
-            if y == 0 and x == 70:
-                i = -1
-                j = 1
-                x = 69
-                y = 1
-            if x == 0:
-                i = 1
-                x = 2
-                y += 1
-            elif y == 0:
-                j = 1
-                x +=1
-                y = 2
-            elif x == 70:
-                i = -1
-                x = 69
-                y -= 1
-            elif y == 22:
-                j = -1
-                x +=1
-                y = 20
+
+        while True:             #while x != 0 and y != 0 and x != 62 and y != 22:
+            
+            print_there(x, y, '\033[93m' + '⏺')
+            sleep(0.01)
+            print_there( x, y, '\033[93m' + ' ')        
+            x += i
+            y += j
+            
+            # Next moment  #---------------------------------------#-----------------------------#
+            if (y + j) <= 0:
+                if (x + i) >= 60:
+                    i = -i
+                j = -j
+            elif (y + j) in Bricks.obj_y:
+                obj_num = int((y//2 - 1)*10 + (x // 6))
+                print('x',x,'y',y,obj_num,end='')
+                if obj_num == 39:
+                    print('in 39',bricks[39].status,end='')
+                    if bricks[39].status == 'ON':
+                        bricks[39].paint_bricks(39, mode=1)
+                        j = -j
+                        continue
+                else: 
+                    if bricks[obj_num].status == 'ON':
+                        print(obj_num,'not in 39!!!',end='')
+                        bricks[obj_num].paint_bricks(obj_num,mode=1) 
+                        j = -j
+                #sleep(30)
+            elif (y + j) == 22:
+                j =- j                      #delete!!!!! 
+            elif (x + i) <= 0 or (x + i) >= 60:
+                if (y + j) <= 0:
+                    j = -j
+                i = -i
+                
+            #elif (x + i) in Bricks.obj_x:
+                
   
 
 
@@ -129,11 +159,13 @@ class Ball:
 def main():
     os.system('clear')
     os.system('setterm -cursor off')  
-    ball_1 = Ball(22, 12, 0.1)
-    paddle = Paddle(10, 3, 10)
-    objects_x, objects_y      = bricks(1)
-    paddle.paint()
-    ball_1.ball_deamon(objects_x, objects_y)
+    ball_1 = Ball(56, 10, 0.1)                      
+    paddle = Paddle(10, 3, 10)              #create paddle
+    paddle.paint()                      #draw paddle
+    bricks = [Bricks() for i in range(40)]          #create bricks
+    for i in range(40):
+        bricks[i].paint_bricks(i,mode=0)                  #draw bricks                   
+    ball_1.ball_deamon(bricks)            #balls_thread
     while True:
             event = getChar()
             if event == '[D':
