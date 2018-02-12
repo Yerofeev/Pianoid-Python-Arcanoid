1d63f37ac444d8b80f09ce39e3eb66afce65ef3c
Erofeev Egor <illuziya@mail.ru>
2018-02-12 19:34:28 +0300

    prized added problem - prized are not displayed properly while falling

--- arcanoid.py
+++ arcanoid.py
@@ -13,8 +13,13 @@ from random import randint
 
 colors = ['\033[91m', '\033[93m', '\033[32m', '\033[34m', '\033[37m', '\033[35m', '\033[96m']  #[red,yellow,green,blue,white,purple,light blue] #Extented ANSI
 M = [[1 for x in range(63)] for y in range(18)]
-level_1 = [[i%2] if (i//12)%2==0 else [(i-1)%2] for i in range(84)]
-prizes = ['⧯','⏺']
+color_level_1 = [[i%2] if (i//12)%2==0 else [(i-1)%2] for i in range(84)]
+prizes_level_1 = [0 if x not in [76,77,80] else 1 for x in range(84)]
+prizes_level_1[75] = 1; prizes_level_1[77] = 2; prizes_level_1[80] = 3
+
+prizes = {}
+balls = {}
+
 def print_there(x,y,text):   
    sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (y, x, text))
    sys.stdout.flush()
@@ -44,9 +49,7 @@ def getChar():
             if answer == '4':
                 return answer
             if answer == 'q':
-                 print('Quit')
-                 sleep(0.2)
-                 sys.exit()                 
+                 sys.exit()
             else:  break
         finally:
             termios.tcflush(sys.stdin, termios.TCIOFLUSH)
@@ -61,13 +64,11 @@ def preliminaries():
         print_there(0,i+1,'\033[32m'+chr(9608)+ ' '*60 + chr(9608) + '\033[0m')
 
 
-
-
 class Prize():
 
     def __init__(self, i ,x ,y, paddle,bricks):
-        prizes = {}
-        self.prize_type = i
+        prizes_types = ['⟺', '⏺', '⏹']  #    9830'♥'   🕯 '🚀'   '⟺'    '❤ '
+        self.prize_type = prizes_types[i-1]
         self.prize_x = x
         self.prize_y = y
         self.threading = threading.Thread(target=self.prize, args=(self.prize_type,self.prize_x,self.prize_y, paddle,bricks))
@@ -78,24 +79,25 @@ class Prize():
         self.threading.start()
 
     def prize(self,prize_type,x,y,paddle,bricks):
-        if prize_type == 80:
-
             while y != 21:
-                print_there(x+2,y,'⧯')
+                print_there(x+1,y,self.prize_type)
                 sleep(0.1)
-                print_there(x+2,y,' ')
+                print_there(x+1,y,' ')
                 y += 1
-            paddle.length += 2
-            paddle.paint()
-        elif prize_type  == 75:
-            while y != 21:
-                print_there(x+2,y,'⏺')
-                sleep(0.1)
-                print_there(x+2,y,' ')
-                y += 1
-
-            ball_2 = Ball (34, 20, 0.1)
-            ball_2.ball_deamon(bricks, paddle)
+            if self.prize_type == '⏹':
+                paddle.length -= 2
+                paddle.paint()
+            elif self.prize_type == '⟺':   # chr(10231)
+                paddle.length += 2
+                paddle.paint()
+            elif self.prize_type == '⏺':
+                while True:
+                    i = 1
+                    if i not in balls:
+                        balls[i] = Ball(x+1, y-1, 0.1)
+                        balls[i].ball_deamon(bricks, paddle)
+                        break
+                    i += 1
 
 class Bricks():
     
@@ -103,8 +105,9 @@ class Bricks():
         #Bricks.obj_x = [x for x in range(2,62) if x not in range(7,62,6)]
         #Bricks.obj_y = [y for y in range(1,9)]
         self.status = 'ON'
-        self.color = colors[level_1[i][0]]
-        self.prize = 'NO'
+        self.color = colors[color_level_1[i][0]]
+        self.number = i
+        self.prize = prizes_level_1[i]
     
     def paint_bricks(self, paddle, bricks, i=0, x=0, y=0, mode=0):
         if mode != 0:
@@ -114,29 +117,14 @@ class Bricks():
             if M[y-1][x] != 0: return
             self.status = 'OFF'
             print_there (x , y-1 , ' '*5)        #chr(9608)'█'
-            if i == 75:
-                prize_0 = Prize(i, x, y-1, paddle, bricks)
-                prize_0.prize_daemon()
-            if i == 80:
-                prize_1 = Prize(i, x, y-1,paddle, bricks)
-                prize_1.prize_daemon()
-            #print(M)
-            #   Bricks.obj_x.remove(k)
-            #Bricks.obj_y.remove((i//10+1)*2); Bricks.obj_y.remove((i//10+1)*2-1)            
+            if self.prize != 0:
+
+                prizes[i] = Prize(self.prize, x, y-1, paddle, bricks)
+                prizes[i].prize_daemon()
+                self.prize = 0
             return
         print_there((i%12)*5+2, i//12 + 2, self.color + chr(9608) + chr(9608)*3 + chr(9608))
 
-                #print_there (i*5+2, 2+j,(lambda x: '\033[35m'  if (i+j)%2==0 else  '\033[93m')(i) + chr(9608) + chr(9608)*3 + chr(9608) )
-        #print_there (2+i*5, 3+j*2, '\033[93m' + chr(9608) + chr(9608)*3 + chr(9608) )        #chr(9608)'█'
-
-    def delete_brick(self,x,y,i):
-
-        print_there (-2+i%10*7, (i//10+1)*2-1,  ' '*6)
-        print_there (-2+i%10*7, (i//10+1)*2,  ' '*6)        
-        #print((i//10+1)*2,(i//10+1)*2-1)
-        #sleep(30)
-        return
-        
         
 class Paddle:
 
@@ -154,7 +142,7 @@ class Paddle:
     def paint(self):
         print_there(0, 22, ' '*80)                   #SORT OUT  LATER!!!
         for i in range(self.length): 
-            print_there(self.position+i, 22, '\033[32m' + chr(10735) )                 #chr(8718) ∎  #chr(10735)⧯       chr(9209) ' ⏹'
+            print_there(self.position+i, 22, '\033[32m' + chr(10735) )                 #chr(8718) ∎  #chr(10735)⧯       chr(9209) '⏹'
         print_there(  70, 22, '\033[91m' + '❤ '*self.lives) #FINISH LATER!!! 
         
 
@@ -163,15 +151,15 @@ class Ball:
     def __init__(self, x, y, speed):
         self.x = x
         self.y = y
-        self.speed = speed
+        Ball.speed = speed
         
     def ball_deamon(self, bricks, paddle):
         self.threading = threading.Thread(target=self.ball_display, args=(self.x, self.y, bricks,paddle))
         self.threading.daemon = True
         self.threading.start()       
     
-    def ball_set_speed(self, speed):
-        self.speed = speed
+    def ball_set_speed(speed):
+        Ball.speed = speed
 
     def ball_display(self,x,y, bricks, paddle):
         i, j = 1, -1
@@ -180,7 +168,7 @@ class Ball:
             x += i
             y += j  # position of the ball in the next moment
             print_there(x, y,  '⏺')
-            sleep(self.speed)
+            sleep(Ball.speed)
             print_there( x, y, '\033[93m' + ' ')        
 
             
@@ -203,7 +191,7 @@ class Ball:
                             obj_num = (y - 2) * 12 + (x-2+i) // 5            # butt of the brick
                             #print('x', x, 'y', y,'objx', obj_num,' ', end='')
                             bricks[obj_num].paint_bricks(paddle, bricks, x=(((x-2+i)//5)*5)+2, y=y+1, i=obj_num,mode=1)    #y+1 on our line
-                            sleep(self.speed/2)
+                            sleep(Ball.speed/2)
                             i = -i
                         obj_num = (y - 3)*12 + (x-2)//5
                         #print('x', x, 'y', y,'objy',obj_num,' ',end='')
@@ -232,13 +220,13 @@ def main():
    # start_time = time.time()
     preliminaries()
    # print("--- %s seconds ---" % (time.time() - start_time))
-    ball_1 = Ball (12, 15, 0.1)
     paddle = Paddle(10, 3, 10)              #create paddle
     paddle.paint()                      #draw paddle
     bricks = [Bricks(i) for i in range(84)]          #create bricks
     for i in range(84):
         bricks[i].paint_bricks(paddle, bricks, i=i,mode=0)                  #draw bricks   # sort out paddle
-    ball_1.ball_deamon(bricks, paddle)            #balls_thread
+    balls[0] = Ball(12, 15, 0.1)  # Create ball (x,y,speed)
+    balls[0].ball_deamon(bricks, paddle)            #balls_thread
     while True:
             event = getChar()
             if event == '[D':
@@ -250,13 +238,13 @@ def main():
                 paddle.move(direction)
                 paddle.paint()       
             elif event == '1':
-                ball_1.ball_set_speed(1)
+                Ball.ball_set_speed(1)
             elif event == '2':
-                ball_1.ball_set_speed(0.2)
+                Ball.ball_set_speed(0.2)
             elif event == '3':
-                ball_1.ball_set_speed(0.02)
+                Ball.ball_set_speed(0.02)
             elif event == '4':
-                ball_1.ball_set_speed(0.005)
+                Ball.ball_set_speed(0.005)
 
 
 if __name__=='__main__':
